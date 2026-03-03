046d57a03f80cd06293ada4bd5f12a12f3686e30
Zion Pulapakura <pulapakurazion@gmail.com>
2026-03-03 19:45:40 +1100

    changed event date

--- src/config/event.ts
+++ src/config/event.ts
@@ -1,7 +1,7 @@
 export const EVENT = {
-  startDateTime: new Date("2026-02-05T18:00:00+11:00"),
+  startDateTime: new Date("2026-03-05T18:00:00+11:00"),
 
-  displayDate: "February 5, 2026",
+  displayDate: "March 5, 2026",
   time: "6:00 PM - 7:30 PM",
   venue: "Natural Velocity, Docklands",
 };
